06b4ae2a34e8492772aec867497f8b38a7fb64a6
Olivier Robardet <orobardet@users.noreply.github.com>
2016-02-10 08:25:41 +0100

    Mise à jour valeurs par défaut conf

--- conf.php
+++ conf.php
@@ -1,4 +1,4 @@
 <?php
 $opentrackerctl = "/home/bt/opentracker/opentrackerctl.sh";
-$newTrackerUrl = "http://tracker.bt.ec-chan.com/announce";
+$newTrackerUrl = "http://local-tracker.mine.com/announce";
 $reseedDistantTarget = "";  // Url FTP de la forme : ftp://[user:pass@]host[:port][/path/to/save]
